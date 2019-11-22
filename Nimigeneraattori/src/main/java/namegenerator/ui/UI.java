83d929599f076315b27b770938564a681eab34cc
Ami Surakka <siipis@live.co.uk>
2019-11-22 17:11:25 +0200

    JavaFX:n tuominen projektiin.

--- Nimigeneraattori/src/main/java/namegenerator/ui/UI.java
+++ Nimigeneraattori/src/main/java/namegenerator/ui/UI.java
@@ -1,4 +1,28 @@
 package namegenerator.ui;
 
-public class UI {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class UI extends Application {
+    @Override
+    public void init() throws Exception {
+        super.init();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
